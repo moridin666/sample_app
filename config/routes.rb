72b777b875e6a0dfaa2972d02ec67288107ddb85
Jason Schneider <j.a.schneidr@gmail.com>
2014-03-20 15:05:01 -0400

    layouts updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
+  
+  get "users/new"
+  root 'static_pages#home'
+  match 'static_pages/help', to: 'static_pages#help', via: 'get'
+  match 'static_pages/about', to: 'static_pages#about', via: 'get'
+  match 'static_pages/contact', to: 'static_pages#contact', via: 'get'
+  match 'users/signup', to: 'users#new', via: 'get'
 end
