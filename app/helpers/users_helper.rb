3d8a886181bfc09d31f8c10929c2f76cb3b889e4
Jason Schneider <j.a.schneidr@gmail.com>
2014-04-01 15:48:35 -0400

    finish sign in

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
 	#returns the Gravatar image for the given user.
-	def gravatar_for(user)
+	def gravatar_for(user, options = { size: 50 } )
 		gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
 		image_tag(gravatar_url, alt: user.name, glass: "gravatar")
